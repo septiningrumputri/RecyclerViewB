8ad38a448d96775487974cea5a69dd19393dd5dc
septiningrumputri <xeotietie@gmail.com>
2023-06-12 10:10:24 +0700

    Initial commit

---
+++ app/src/main/java/com/septiningrum_202102248/recyclerviewb/MahasiswaViewHolder.java
@@ -0,0 +1,27 @@
+package com.septiningrum_202102248.recyclerviewb;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MahasiswaViewHolder extends RecyclerView.ViewHolder
+{
+    public ImageView _jkImageView;
+    public TextView _jkTextView;
+    public TextView _jpTextView;
+    public TextView _namaTextView;
+    public TextView _nimTextView;
+
+    public  MahasiswaViewHolder(View itemView)
+    {
+        super(itemView);
+
+        _jkImageView = (ImageView) itemView.findViewById(R.id.jkImageView);
+        _jkTextView = (TextView)itemView.findViewById(R.id.jkTextView);
+        _jpTextView = (TextView)itemView.findViewById(R.id.jkTextView);
+        _namaTextView =(TextView) itemView.findViewById(R.id.namaTextView);
+        _nimTextView = (TextView) itemView.findViewById(R.id.nimTextView);
+    }
+}
